e2c21dca2d4ea805fca241cf71a439070a51f1f7
Mahonri Baldwin <mahonri.baldwin@echostar.com>
2016-06-20 08:58:55 -0700

    production build

--- js/compiled/out/figwheel/connect.cljs
+++
@@ -1,3 +0,0 @@
-(ns figwheel.connect (:require [figwheel.client] [figwheel.client.utils] [mb.ui]))
-(figwheel.client/start {:on-jsload (fn [& x] (if js/mb.ui.on-js-reload (apply js/mb.ui.on-js-reload x) (figwheel.client.utils/log :debug "Figwheel: :on-jsload hook 'mb.ui/on-js-reload' is missing"))), :build-id "dev", :websocket-url "ws://localhost:3449/figwheel-ws"})
-
